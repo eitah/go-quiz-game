35436ea3fc7b5b3fd8acee81594a0b42e5e23c8d
eitah <eli@spantree.net>
2021-09-18 09:14:00 -0500

    Working quiz game through part 1. Grateful i used the go parser

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,5 +17,56 @@ func main() {
 }
 
 func mainErr() error {
+	quizFile := flag.String("in", "./problems.csv", "Path to a quiz file to load with data. In the csv format of question,answer, e.g. 1+2,3")
+	flag.Parse()
+	f, err := os.Open(*quizFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	parsed, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return err
+	}
+
+	var questions []string
+	var answers []string
+	for _, qna := range parsed {
+		for index, item := range qna {
+			if index == 0 {
+				questions = append(questions, item)
+			}
+			if index == 1 {
+				answers = append(answers, item)
+			}
+		}
+	}
+
+	buf := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to the quiz game.")
+
+	var countCorrect int
+	var countWrong int
+	for index, question := range questions {
+		fmt.Printf("Here is your next question. %s\n", question)
+		fmt.Print(">")
+		sentence, err := buf.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		fmt.Printf("You guessed: %s. The right answer was %s\n", strings.TrimSpace(sentence), answers[index])
+		if strings.TrimSpace(sentence) == answers[index] {
+			countCorrect++
+			fmt.Println("Good job!")
+		} else {
+			countWrong++
+			fmt.Println("Keep Trying...")
+		}
+	}
+
+	fmt.Printf("Quiz complete! you scored %d/%d correct, which means you missed %d\n", countCorrect, len(questions), countWrong)
+
 	return nil
 }
