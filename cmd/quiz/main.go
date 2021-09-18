e32e07fea65d2c6a7040e2951765eaf19f1b66dd
eitah <eli@spantree.net>
2021-09-18 11:30:37 -0500

    Cleaner way of handling input and timers in the problem loop

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"errors"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -20,38 +19,13 @@ func main() {
 }
 
 func mainErr() error {
-	timer := time.NewTimer(2 * time.Second)
-
-	quit := make(chan bool)
-	errc := make(chan error)
-	done := make(chan error)
-	defer close(quit)
-	defer close(errc)
-	defer close(done)
-
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-timer.C:
-				fmt.Println("Sorry, time is up!")
-				errc <- errors.New("timer finished")
-			case <-errc:
-				return
-			}
-		}
-	}()
-
-	for err := range errc {
-		if err != nil {
-			return err
-		}
-	}
-
 	quizFile := flag.String("in", "./problems.csv", "Path to a quiz file to load with data. In the csv format of question,answer, e.g. 1+2,3")
 	shouldShuffle := flag.Bool("shuffle", false, "Flag to indicate if you should shuffle quiz qs between runs.")
+	timeLimit := flag.Int("limit", 30, "teh time limit for the quiz in seconds")
 	flag.Parse()
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	f, err := os.Open(*quizFile)
 	if err != nil {
 		return err
@@ -68,6 +42,59 @@ func mainErr() error {
 		rand.Shuffle(len(parsed), func(i, j int) { parsed[i], parsed[j] = parsed[j], parsed[i] })
 	}
 
+	questions, answers := makeQandA(parsed)
+
+	buf := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Welcome to the quiz game. Your questions are timed with limit %d seconds\n", *timeLimit)
+
+	var countCorrect int
+	var countWrong int
+
+	for index, question := range questions {
+
+		fmt.Printf("Here is your #%d question. %s\n", index+1, question)
+		fmt.Print(">")
+
+		answerCh := make(chan string)
+		errCh := make(chan error)
+		go func() {
+			sentence, err := buf.ReadString('\n')
+			if err != nil {
+				errCh <- err
+			}
+			answerCh <- strings.TrimSpace(sentence)
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("Sorry time is up.")
+			countDidNotAnswer := len(questions) - countCorrect - countWrong
+			fmt.Printf("Quiz complete! you scored %d/%d correct. You got %d wrong and did not answer %d questions\n", countCorrect, len(questions), countWrong, countDidNotAnswer)
+			return nil
+		case err := <-errCh:
+			if err != nil {
+				return err
+			}
+		case answer := <-answerCh:
+			fmt.Printf("You guessed: %s. The right answer was %s\n", answer, answers[index])
+			if answer == answers[index] {
+				countCorrect++
+				fmt.Println("Good job!")
+			} else {
+				countWrong++
+				fmt.Println("Keep Trying...")
+			}
+		}
+	}
+
+	countDidNotAnswer := len(questions) - countCorrect - countWrong
+	fmt.Printf("Quiz complete! you scored %d/%d correct, which means you got %d wrong and did not answer %d questions\n", countCorrect, len(questions), countWrong, countDidNotAnswer)
+
+	return nil
+}
+
+func makeQandA(parsed [][]string) ([]string, []string) {
 	var questions []string
 	var answers []string
 	for _, qna := range parsed {
@@ -80,36 +107,5 @@ func mainErr() error {
 			}
 		}
 	}
-
-	buf := bufio.NewReader(os.Stdin)
-
-	fmt.Println("Welcome to the quiz game.")
-
-	var countCorrect int
-	var countWrong int
-	for index, question := range questions {
-		delimiter := "next"
-		if index == 0 {
-			delimiter = "first"
-		}
-		fmt.Printf("Here is your %s question. %s\n", delimiter, question)
-		fmt.Print(">")
-		sentence, err := buf.ReadString('\n')
-		if err != nil {
-			return err
-		}
-		fmt.Printf("You guessed: %s. The right answer was %s\n", strings.TrimSpace(sentence), answers[index])
-		if strings.TrimSpace(sentence) == answers[index] {
-			countCorrect++
-			fmt.Println("Good job!")
-		} else {
-			countWrong++
-			fmt.Println("Keep Trying...")
-		}
-	}
-
-	countDidNotAnswer := len(questions) - countCorrect - countWrong
-	fmt.Printf("Quiz complete! you scored %d/%d correct, which means you got %d wrong and did not answer %d questions\n", countCorrect, len(questions), countWrong, countDidNotAnswer)
-
-	return nil
+	return questions, answers
 }
